b887eddb37efd29784b6e0939d9442a30fed099f
quotuo <81864960+quotuo@users.noreply.github.com>
2023-05-08 15:46:01 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: '不可思议',
   author: '付有司',
   email: 'fuyousi@outlook.my',
-  link: 'https://twitter.com',
+  link: 'https://siyi.eu.org',
   description: '多年以后,小孩将会回想起他将青蛙皮剥下的下午,白色的肉丝带着血',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
